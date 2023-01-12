2923c5e7601dcc1d63375d325adab61bbde168c6
hp <hp@DESKTOP-QAKQ1A6>
2023-01-12 18:20:50 +0600

    Dynamic Module comment

---
+++ AppTest/Areas/Admin/Controllers/ModuleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppTest.Areas.Admin.Controllers
+{
+    public class ModuleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+            // this controller is for Dymaic module >>
+        }
+    }
+}
